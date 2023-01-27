e7624674a540bd55b381daf63d376aa29b6a18cc
wonny2 <onehee2choi@gmail.com>
2023-01-27 22:47:03 +0900

    구조분해할당 test

--- myapp/pages/test/test5.tsx
+++ myapp/pages/test/test5.tsx
@@ -1,24 +1,36 @@
-import { useEffect, useState } from "react"
+import Test6  from "./test6"
+
 
 export default function Text5() {
 
-    const [scrollPosition, SetScrollPosition] = useState(0);
-
-    const onScroll = () => {
-        SetScrollPosition(window.scrollY)
-        console.log(scrollPosition)
-    };
-
-    useEffect(() => {
-        window.addEventListener('scroll', onScroll);
-        return () => {
-            window.removeEventListener("scroll", onScroll)
+    const arr = [
+        {
+            title : "first",
+            age: 1
+        },
+        {
+            title: "second",
+            age: 2,
+        },
+        {
+            title : "third",
+            age : 3
+        },
+        {
+            "title":"fourth",
+            "age":4,
         }
-    },[]) 
-    
+    ];
+
     return(
         <>
-            
+            {arr.map((el,i) => (
+                <Test6 {...el} key={i} />
+                // <div key={i}>
+                //     <div>{el.title}</div>
+                //     <div>{el.age}</div>
+                // </div>
+            ))}
         </>
     )
-}
+}``
